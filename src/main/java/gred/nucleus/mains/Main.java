e1a41d0684a20dcfc1a4e823ee0bbfbb8b420f2c
SafarbatiSami <samsafarbati@hotmail.com>
2023-07-11 16:22:07 +0200

    Created a GUI for NucleusJ2.0

--- src/main/java/gred/nucleus/mains/Main.java
+++ src/main/java/gred/nucleus/mains/Main.java
@@ -1,11 +1,15 @@
 package gred.nucleus.mains;
 
 import gred.nucleus.cli.*;
+import gred.nucleus.dialogs.MainGui;
+
+import ij.IJ;
 import ij.util.ThreadUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import javax.swing.*;
 import java.lang.invoke.MethodHandles;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,7 +22,7 @@ public class Main {
 		List<String> listArgs = Arrays.asList(args);
 		
 		/* Allow IJ threads from thread pool to timeout */
-		ThreadUtil.threadPoolExecutor.allowCoreThreadTimeOut(true);
+		//ThreadUtil.threadPoolExecutor.allowCoreThreadTimeOut(true);
 		
 		if (listArgs.contains("-h") || listArgs.contains("-help")) {
 			CLIHelper.run(args);
@@ -26,12 +30,19 @@ public class Main {
 			CLIActionOptionOMERO command = new CLIActionOptionOMERO(args);
 			CLIRunActionOMERO cliOMERO = new CLIRunActionOMERO(command.getCmd());
 			cliOMERO.run();
-		} else {
+		} else if (listArgs.contains("-nj") || listArgs.contains("-cli") || listArgs.contains("-CLI")){
 			CLIActionOptionCmdLine command = new CLIActionOptionCmdLine(args);
 			CLIRunAction cli = new CLIRunAction(command.getCmd());
 			cli.run();
+		} else {
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					MainGui gui   = new MainGui();
+					gui.setVisible(true);
+				}
+			});
 		}
-		ThreadUtil.threadPoolExecutor.shutdown();
+		//ThreadUtil.threadPoolExecutor.shutdown();
 	}
 	
 }
