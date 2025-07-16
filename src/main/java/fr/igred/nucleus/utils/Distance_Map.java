6740edbcfcbc3d2e9c44e11b82b13a44b1b1d421
Pierre Pouchin <pierre.pouchin@uca.fr>
2025-07-16 23:44:59 +0200

    Add header

--- src/main/java/fr/igred/nucleus/utils/Distance_Map.java
+++ src/main/java/fr/igred/nucleus/utils/Distance_Map.java
@@ -1,3 +1,20 @@
+/*
+ * NucleusJ
+ * Copyright (C) 2014-2025 iGReD
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.igred.nucleus.utils;
 
 import ij.IJ;
@@ -9,52 +26,6 @@ import ij.process.ImageProcessor;
 import ij.process.StackConverter;
 
 
-/* Bob Dougherty 8/8/2006
-Saito-Toriwaki algorithm for Euclidean Distance Transformation.
-Direct application of Algorithm 1.
-Version S1A: lower memory usage.
-Version S1A.1 A fixed indexing bug for 666-bin data set
-Version S1A.2 Aug. 9, 2006.  Changed noResult value.
-Version S1B Aug. 9, 2006.  Faster.
-Version S1B.1 Sept. 6, 2006.  Changed comments.
-Version S1C Oct. 1, 2006.  Option for inverse case.
-                           Fixed inverse behavior in y and z directions.
-Version D July 30, 2007.  Multithreaded processing for step 2.
-
-This version assumes the input stack is already in memory, 8-bit, and
-outputs to a new 32-bit stack.  Versions that are more stingy with memory
-may be forthcoming.
-
- License:
-	Copyright (c) 2006, OptiNav, Inc.
-	All rights reserved.
-
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions
-	are met:
-
-		Redistributions of source code must retain the above copyright
-	notice, this list of conditions and the following disclaimer.
-		Redistributions in binary form must reproduce the above copyright
-	notice, this list of conditions and the following disclaimer in the
-	documentation and/or other materials provided with the distribution.
-		Neither the name of OptiNav, Inc. nor the names of its contributors
-	may be used to endorse or promote products derived from this software
-	without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-	A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
 public class Distance_Map implements PlugInFilter {
 	private static final int THRESHOLD = 126;
 	
